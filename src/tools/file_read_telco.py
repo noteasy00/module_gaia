616d8e126408f3399c20e3bb71bb771a590ebad3
hyemi <oham9990@gmail.com>
2026-03-29 19:48:10 +0900

    file_read

--- src/tools/file_read_telco.py
+++ src/tools/file_read_telco.py
@@ -5,50 +5,34 @@ from sklearn.preprocessing import LabelEncoder
 model = joblib.load("model.pkl")
 preprocessor = joblib.load("prepocessor.pkl")
 
-# 전처리 저장했을 경우
-def predict_from_csv(file_path):
+
+def predict_from_csv(file_path, bundle_path="model_bundle.pkl"):
+    # bundle 로드
+    bundle = joblib.load(bundle_path)
+
+    preprocessor = bundle['preprocessor']
+    model = bundle['model']
+    threshold = bundle['threshold']
+    feature_names = bundle['feature_columns']
+
+    # CSV 파일 읽기
     df = pd.read_csv(file_path)
+    df = df.drop(columns=["customerID"], errors="ignore")
 
-    X = df.drop('customerID', axis=1, errors='ignore')
+    # 컬럼 정렬
+    df=df.reindex(columns=feature_names, fill_value=0)
 
-    X_processed = preprocessor.transform(X)
+    # 전처리 적용
+    X_processed = preprocessor.transform(df)
 
-    predictions = model.predict(X_processed)
+    # 예측
+    probabilities = model.predict_proba(X_processed)[:, 1]
+    predictions = (probabilities >= threshold).astype(int)
 
-    df['prediction'] = predictions
+    return predictions, probabilities
 
-    return df
-
-
-# 전처리 필요한 경
-def predic_from_csv(file_path):
-    df = pd.read_csv(file_path)
-
-    X=df.copy()
-    X = X.drop('customerID', axis=1, errors='ignore')
-
-    # 이진형 데이터 변환 (Label Encoding)
-    binary_cols = ['PaperlessBilling']
-
-    le = LabelEncoder()
-    for col in binary_cols:
-        X[col] = le.fit_transform(X[col])
-
-    # 다중 클래스 범주 데이터 변환 (One-Hot Encoding)
-    multi_cols = ['InternetService',
-                   'TechSupport',
-                   'StreamingMovies',
-                  'Contract', 
-                  'PaymentMethod'
-    ]
-    X = pd.get_dummies(X, columns=multi_cols, drop_first=True)
-
-    # 컬럼 맞추기
-    model_colums = model. feature_names_in_
-    X = X.reindex(columns = model_colums, fill_value=0)
-
-    predictions = model.predict(X)
-
-    df['prediction'] = predictions
-
-    return df
+if __name__ == "__main__":
+    file_path = "test_data.csv"  # 예측할 CSV 파일 경로
+    predictions, probabilities = predict_from_csv(file_path)
+    print("Predictions:", predictions)
+    print("Probabilities:", probabilities)
